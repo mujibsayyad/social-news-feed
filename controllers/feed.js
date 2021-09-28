14bec5d862a4e2910d966eb89f25a9e7fcd47bb2
themujib <mujibsayyad97@gmail.com>
2021-09-28 23:02:19 +0530

    Initial Commit

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,10 +1,22 @@
+const TimeAgo = require('javascript-time-ago');
+const en = require('javascript-time-ago/locale/en.json');
+
+TimeAgo.addDefaultLocale(en);
+
+// Create formatter (English).
+const timeAgo = new TimeAgo('en-US');
+
 const posts = [];
 
 exports.getIndex = (req, res, next) => {
+  updatedPosts = posts.map((post) => {
+    return { ...post, added: timeAgo.format(post.added) };
+  });
+
   res.render('index', {
     pageTitle: 'NewsFeed',
     path: '/',
-    post: posts,
+    post: updatedPosts,
   });
 };
 
@@ -16,7 +28,12 @@ exports.getPost = (req, res, next) => {
 };
 
 exports.postAddPost = (req, res, next) => {
-  posts.unshift({ title: req.body.title, added: new Date() });
+  posts.unshift({
+    title: req.body.title,
+    added: new Date(),
+  });
 
   res.redirect('/');
+
+  console.log(posts);
 };
